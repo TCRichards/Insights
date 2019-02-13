67606256a19c67b0296b5a47c80a984d4a3c9893
Thomas Richards <thomas.c.richards@outlook.com>
2019-02-13 14:34:01 -0500

    fixed errors from screen

--- screen.py
+++ screen.py
@@ -1,9 +1,13 @@
 from pygaze import libtime
 from pygaze import display, screen, keyboard
 from pygaze.eyetracker import EyeTracker
+from win32api import GetSystemMetrics
+
 
 import random
 
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
 # # # # #
 # prep
 
@@ -11,22 +15,27 @@ import random
 libtime.expstart()
 
 # create keyboard object
-keyboard = keyboard.Keyboard()
+kb = keyboard.Keyboard()
 
 # display object
 disp = display.Display()
+scr = screen.Screen()
 
+MESSAGE = 'HELLO WORLD'
+'''
+Wait to initilaze eye tracker until plugged in
 # create eyelink object
 eyetracker = EyeTracker(disp)
 
 # eyelink calibration
 eyetracker.calibrate()
+'''
 
 while kb.get_key()[0] == None:
 	# colour
 	col = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 	# position
-	pos = (random.randint(0,DISPSIZE[0]), random.randint(0,DISPSIZE[1]))
+	pos = (random.randint(0,width), random.randint(0,height))
 	# text
 	scr.draw_text(text=MESSAGE, colour=col, pos=pos, fontsize=84)
 	# display
